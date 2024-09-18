88cae07d97bafbf1844f8adea986fb975083fca8
LeoIrv50 <leo.irv.oliver.02@gmail.com>
2024-09-18 13:08:49 -0500

    Initial commit

---
+++ src/main/java/com/prestamo/DawiSemana01Application.java
@@ -0,0 +1,13 @@
+package com.prestamo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiSemana01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DawiSemana01Application.class, args);
+    }
+
+}
